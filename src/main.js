95f8df7c3a71a8a447825a2ab229ec0957d2d324
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-08-02 19:12:36 -0500

    :$ pruebitas

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import ghibli from './data/ghibli/ghibli.js';
 
 //document.getElementById("tittle").innerText = "La producccion es" + movies.studio;
 //let prueba = document.getElementById("root").innerText
-console.log(example, anotherExample)
+//console.log(example, anotherExample)
 
 //Obteniendo el arreglo con 20 elementos
 let films = ghibli.films;
-console.log(films);
+// console.log(films);
 
 // Obteniendo los títulos del objeto films en un arreglo de 20 elementos
 // let titulo = films.map(obj => {
@@ -26,9 +26,30 @@ let filmsGhibli = films.map(obj => {
     document.getElementById("root").innerHTML +=
         `
         <div class="card">
-        <h1> "${obj.title}" </h1>
+        <h1> ${obj.title} </h1>
         <img class="cardPoster" src="${obj.poster}"/>
-        <button id="botonn" class="infoButton" onClick="pruebita()">Descubre más</button>
+        <button class="botonn">Descubre más</button>
+        
         </div>
         `
 })
+
+// let botones = document.getElementsByClassName("botonn");
+// let aaa = document.getElementsByClassName("infoCard");
+// for (let cont = 0; cont < aaa.length; cont++) {
+//     aaa[cont]
+// }
+// console.log(aaa);
+
+// //for (let uwu = 0; uwu < sinopsis.length; uwu++) {
+//     //console.log(sinopsis);
+// //}
+
+
+
+//     for (let i = 0; i < botones.length; i++) {
+//         botones[i].onclick = function () {
+//             aaa.style = "display: none";
+//         }
+//     }
+
