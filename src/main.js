5ad936d799d36e26b3fb469f1ff2b8c13eed8cc5
Fernanda Alvarez <mariafernandalvarezramirez@gmail.com>
2022-08-08 18:26:49 -0500

    se añadió director

--- src/main.js
+++ src/main.js
@@ -36,26 +36,38 @@ function orderByLetterZ(a, b) {
     }
 }
 
+let yearSelection = document.getElementById("movieyears");
+yearSelection.addEventListener("change", function () {
+    cleanCards();
+    const moviesByYear = obtainingFilms.filter(function filterMovies(movies) {
+        if (movies.release_date <= "1989" && yearSelection.value == "80s" || movies.release_date >= "1990" && movies.release_date <= "1999" && yearSelection.value == "90s" || movies.release_date >= "2000" && yearSelection.value == "00s")
+            return movies.release_date;
+    })
+    showCards(moviesByYear);
+});
+
+let filmByDirector = document.getElementById("filmdirector");
+filmByDirector.addEventListener("change", function () {
+    cleanCards();
+    const moviesByDirector = obtainingFilms.filter(function filterDirector(movies) {
+        if (movies.director == "Hayao Miyazaki" && filmdirector.value == "Hayao" || movies.director == "Isao Takahata" && filmdirector.value == "Isao" || movies.director == "Yoshifumi Kondō" && filmdirector.value == "Yoshifumi" || movies.director == "Gorō Miyazaki" && filmdirector.value == "Goro" || movies.director == "Hiromasa Yonebayashi" && filmdirector.value == "Hiromasa")
+            return movies.director;
+    })
+    showCards(moviesByDirector);
+})
+
 let dropDownMenu = document.getElementById("sortingMenu");
 dropDownMenu.addEventListener("change", function () {
     if (dropDownMenu.value === "AZ") {
         cleanCards();
         let filmsFromAtoZ = obtainingFilms.sort(orderByLetterA);
+        console.log(obtainingFilms);
         return showCards(filmsFromAtoZ);
     }
     if (dropDownMenu.value === "ZA") {
         cleanCards();
         let filmsFromZtoA = obtainingFilms.sort(orderByLetterZ);
+        console.log(obtainingFilms)
         return showCards(filmsFromZtoA);
     }
-});
-
-let yearSelection = document.getElementById("movieyears");
-yearSelection.addEventListener("change", function () { 
-    cleanCards();
-    const moviesByYear = obtainingFilms.filter(function filterMovies(movies) {
-        if (movies.release_date <= "1989" && yearSelection.value == "80s" || movies.release_date >= "1990" && movies.release_date <= "1999" && yearSelection.value == "90s" || movies.release_date >= "2000" &&  yearSelection.value == "00s")
-            return movies.release_date;
-    })
-    showCards(moviesByYear);
 });
