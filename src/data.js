bdd8379234ac515082b0abf0ce948016c73b8d3a
SaumarDev <saumar_1@hotmail.com>
2022-08-11 11:05:54 -0500

    OH regional

--- src/data.js
+++ src/data.js
@@ -1,9 +1,46 @@
+import ghibli from './data/ghibli/ghibli.js';
+
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+let obtainingFilms2 = ghibli.films;
+
+export const moviesFromEighties = obtainingFilms2.filter(a => a.release_date <= "1989");
+export const moviesFromNineties = obtainingFilms2.filter(a => a.release_date <= "1999" && a.release_date >= "1990");
+export const recentMovies = obtainingFilms2.filter(a => a.release_date >= "2000");
+
+export function filterByreleaseDate(year) {
+    if (year == "80s") {
+        return obtainingFilms2.filter(a => a.release_date <= "1989")
+    }
+    if (yearSelection.value == "90s") {
+        cleanCards();
+        return showCards(moviesFromNineties);
+    }
+    if (yearSelection.value == "00s") {
+        cleanCards();
+        return showCards(recentMovies);
+    }
+}
+
+
+export function orderByLetterA(a, b) {
+    if (a.title < b.title) {
+        return -1;
+    }
+}
+
+export function orderByLetterZ(a, b) {
+    if (a.title > b.title) {
+        return -1;
+    }
+}
+export function filterByDirector(director) {
+    return obtainingFilms2.filter(a => a.director == director)
+}
--- src/main.js
+++ src/main.js
@@ -1,62 +1,92 @@
-// import { example, anotherExample } from './data.js';
+import { recentMovies, moviesFromNineties, moviesFromEighties, orderByLetterA, orderByLetterZ, filterByDirector } from './data.js';
 import ghibli from './data/ghibli/ghibli.js';
 
 // Obteniendo el arreglo con 20 elementos
 const obtainingFilms = ghibli.films;
-// console.log(films);
 
+// Aquí con .map() logramos llamar cada elemento (la -propiedad-) dentro del arreglo
 function showCards(films) {
-   return films.map(obj => {
+    return films.map(obj => {
         document.getElementById("root").innerHTML +=
             `
             <div class="card">
             <h1>${obj.title}</h1>
             <img class="cardPoster" src="${obj.poster}"/>
+            <p>Año: ${obj.release_date}</p>
+            <p>Director: ${obj.director}</p>
+            <p>Puntuación: ${obj.rt_score}</p>
             <button class="moreInfoButton">Conoce más</button>
-            
             </div>
             `
     });
 }
-
-function cleanCards (){
-    document.getElementById("root").innerHTML = "";
-}
-
 let prueba = showCards(obtainingFilms);
-// Aquí con .map() logramos llamar cada elemento (la -propiedad-) dentro del arreglo
-// let showingCards = obtainingFilms.map(obj => {
-//     document.getElementById("root").innerHTML +=
-//         `
-//         <div class="card">
-//         <h1>${obj.title}</h1>
-//         <img class="cardPoster" src="${obj.poster}"/>
-//         <button class="moreInfoButton">Conoce más</button>
-//         </div>
-//         `
-// });
 
-function orderByLetterA(a, b) {
-    if (a.title < b.title) {
-        return -1;
-    }
-}
-function orderByLetterZ(a, b) {
-    if (a.title > b.title) {
-        return -1;
-    }
-}
+const cleanCards = () => document.getElementById("root").innerHTML = "";
 
-let dropDownMenu = document.getElementById("sortingMenu");
-dropDownMenu.addEventListener("change", function() {
-    if (dropDownMenu.value === "AZ") {
+let yearSelection = document.getElementById("movieyears");
+yearSelection.addEventListener("change", function () {
+    if (yearSelection.value == "80s") {
         cleanCards();
-        let filmsFromAtoZ = obtainingFilms.sort(orderByLetterA);
-        return showCards(filmsFromAtoZ);
+        return showCards(filterByreleaseDate(yearSelection.value));
     }
-    if (dropDownMenu.value === "ZA") {
+    if (yearSelection.value == "90s") {
         cleanCards();
-        let filmsFromZtoA = obtainingFilms.sort(orderByLetterZ);
-        return showCards(filmsFromZtoA);
-    } 
+        return showCards(moviesFromNineties);
+    }
+    if (yearSelection.value == "00s") {
+        cleanCards();
+        return showCards(recentMovies);
+    }
 });
+
+/*let filmByDirector = document.getElementById("filmdirector");
+filmByDirector.addEventListener("change", function () {
+    cleanCards();
+    const moviesByDirector = obtainingFilms.filter(function filterDirector(movies) {
+        if (movies.director == "Hayao Miyazaki" && filmByDirector.value == "Hayao" || movies.director == "Isao Takahata" && filmByDirector.value == "Isao" || movies.director == "Yoshifumi Kondō" && filmByDirector.value == "Yoshifumi" || movies.director == "Gorō Miyazaki" && filmByDirector.value == "Goro" || movies.director == "Hiromasa Yonebayashi" && filmByDirector.value == "Hiromasa")
+            return movies.director;
+    })
+    showCards(moviesByDirector);
+})*/
+
+
+let filmByDirector = document.getElementById("filmdirector");
+filmByDirector.addEventListener("change", function () {
+    if (filmByDirector.value == "Hayao") {
+        cleanCards();
+        return showCards(filterByDirector("Hayao Miyazaki"));
+    }
+    if (filmByDirector.value == "Isao") {
+        cleanCards();
+        return showCards(filterByDirector("Isao Takahata"));
+    }
+    if (filmByDirector.value == "Yoshifumi") {
+        cleanCards()
+        return showCards(filterByDirector("Yoshifumi Kondō"))
+    }
+    if (filmByDirector.value == "Goro") {
+        cleanCards()
+        return showCards(filterByDirector("Gorō Miyazaki"));
+    }
+    if (filmByDirector.value == "Hiromasa") {
+        cleanCards()
+        return showCards(filterByDirector("Hiromasa Yonebayashi"));
+    }
+});
+
+    let dropDownMenu = document.getElementById("sortingMenu");
+    dropDownMenu.addEventListener("change", function () {
+        if (dropDownMenu.value === "AZ") {
+            cleanCards();
+            let filmsFromAtoZ = obtainingFilms.sort(orderByLetterA);
+            console.log(obtainingFilms);
+            return showCards(filmsFromAtoZ);
+        }
+        if (dropDownMenu.value === "ZA") {
+            cleanCards();
+            let filmsFromZtoA = obtainingFilms.sort(orderByLetterZ);
+            console.log(obtainingFilms)
+            return showCards(filmsFromZtoA);
+        }
+    });
